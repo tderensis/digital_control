d1953eb3087e063f45995e7c677131079caab322
Tom DeRensis <tderensis@gmail.com>
2017-10-26 23:48:30 -0400

    Add simulation for full state feedback tracking system.

--- control_sim.py
+++ control_sim.py
@@ -100,3 +100,57 @@ def sim_regob(phi, gamma, C, L, K, T, x0, sim_time):
     y = np.transpose(y)
 
     return (t, u, x, xhat, y)
+
+def sim_tssf(phi, gamma, C, phia, gammaa, L1, L2, T, x0, ref, sim_time):
+    """ Simulate the response of the full order observer regulator.
+
+    Args:
+        phi (matrix): The discrete plant model A matrix
+        gamma (matrix): The discrete plant model B matrix
+        C (matrix): The output matrix
+        phia (matrix): The discrete additional dynamics A matrix
+        gammaa (matrix): The discrete additional dynamics B matrix
+        L1 (matrix): The state gain matrix
+        L2 (matrix): The additional dynamics gain matrix
+        T: The sampling interval in seconds
+        x0 (matrix): The initial state
+        ref (matrix): The reference input (target)
+        sim_time: The time to simulate to in seconds
+
+    Returns:
+        tuple: (t, u, x, y) Where t is the time array, u are the inputs,
+            x are the state variables, xhat are the estimated states, and y
+            are the outputs.
+    """
+
+    t = np.arange(0, sim_time, T)
+    num_samples = len(t)
+    (num_states, num_inputs) = gamma.shape
+    (num_outputs, num_states) = C.shape
+
+    x = np.matrix(np.zeros((num_states, num_samples)))
+    xa = np.matrix(np.zeros((gammaa.shape[0], num_samples)))
+    u = np.matrix(np.zeros((num_inputs, num_samples)))
+    y = np.matrix(np.zeros((num_outputs, num_samples)))
+
+    initial = np.matrix(x0)
+    if initial.shape[0] != num_states:
+        x[:, 0] = np.transpose(initial)
+    else:
+        x[:, 0] = initial
+
+    y[:, 0] = C * x[:, 0]
+    u[:, 0] = -L1 * x[:, 0] + L2 * xa[:,0]
+
+    for i in range(1, num_samples):
+        x[:, i] = phi * x[:, i-1] + gamma * u[:, i-1]
+        xa[:, i] = phia * xa[:, i-1] + gammaa * (ref[:, i-1] - y[:, i-1])
+        y[:, i] = C * x[:, i]
+        u[:, i] = -L1 * y[:, i] + L2 * xa[:, i]
+
+    # Be consistent and return matrices that are the same shape np.step() would return
+    x = np.transpose(x)
+    u = np.transpose(u)
+    y = np.transpose(y)
+
+    return (t, u, x, y)
